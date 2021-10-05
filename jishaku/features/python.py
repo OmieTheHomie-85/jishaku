4ad691863e30dda871544e5aad282ea730fb8976
OmieTheHomie-85 <69497376+OmieTheHomie-85@users.noreply.github.com>
2021-10-04 18:18:59 -0600

    Update python.py

--- jishaku/features/python.py
+++ jishaku/features/python.py
@@ -13,8 +13,8 @@ The jishaku Python evaluation/execution commands.
 
 import io
 
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 
 from jishaku.codeblocks import codeblock_converter
 from jishaku.exception_handling import ReplResponseReactor
@@ -84,13 +84,13 @@ class PythonFeature(Feature):
         What you return is what gets stored in the temporary _ variable.
         """
 
-        if isinstance(result, discord.Message):
+        if isinstance(result, nextcord.Message):
             return await ctx.send(f"<Message <{result.jump_url}>>")
 
-        if isinstance(result, discord.File):
+        if isinstance(result, nextcord.File):
             return await ctx.send(file=result)
 
-        if isinstance(result, discord.Embed):
+        if isinstance(result, nextcord.Embed):
             return await ctx.send(embed=result)
 
         if isinstance(result, PaginatorInterface):
@@ -113,7 +113,7 @@ class PythonFeature(Feature):
             # Since this avoids escape issues and is more intuitive than pagination for
             #  long results, it will now be prioritized over PaginatorInterface if the
             #  resultant content is below the filesize threshold
-            return await ctx.send(file=discord.File(
+            return await ctx.send(file=nextcord.File(
                 filename="output.py",
                 fp=io.BytesIO(result.encode('utf-8'))
             ))
@@ -184,7 +184,7 @@ class PythonFeature(Feature):
                         text = "\n".join(lines)
 
                         if use_file_check(ctx, len(text)):  # File "full content" preview limit
-                            send(await ctx.send(file=discord.File(
+                            send(await ctx.send(file=nextcord.File(
                                 filename="inspection.prolog",
                                 fp=io.BytesIO(text.encode('utf-8'))
                             )))
@@ -210,7 +210,7 @@ class PythonFeature(Feature):
             text = "\n".join(disassemble(argument.content, arg_dict=arg_dict))
 
             if use_file_check(ctx, len(text)):  # File "full content" preview limit
-                await ctx.send(file=discord.File(
+                await ctx.send(file=nextcord.File(
                     filename="dis.py",
                     fp=io.BytesIO(text.encode('utf-8'))
                 ))
